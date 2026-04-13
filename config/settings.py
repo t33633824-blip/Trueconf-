c608c0c14a5288ec22b6aa951001f0d2c7b25945
t33633824-blip <t33633824@gmail.com>
2026-04-13 21:36:07 +0300

    settings.py

---
+++ config/settings.py
@@ -0,0 +1,19 @@
+# Telegram
+API_ID = 12345678
+API_HASH = "ВАШ_API_HASH"
+SESSION_NAME = "sessions/tg_session"
+
+CHANNELS = [
+    "@channel_name_1",
+    "@channel_name_2",
+]
+
+# TrueConf
+TC_SERVER = "https://ВАШ_TRUECONF_СЕРВЕР"
+TC_CLIENT_ID = "ВАШ_CLIENT_ID"
+TC_CLIENT_SECRET = "ВАШ_CLIENT_SECRET"
+TC_TOKEN_TTL = 3540
+
+# Логирование
+LOG_LEVEL = "INFO"
+LOG_FILE = "logs/bot.log"
